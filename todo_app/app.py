760a8b6cb7d61a4fc9b4cf7873cd6ded5579860f
DANI PHILIP <DaniPhilip5@outlook.com>
2021-11-29 18:59:13 +0000

    commit

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-
+from flask import Flask, render_template, request,redirect
+from todo_app.data.session_items import get_items
+from todo_app.data.session_items import add_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -8,4 +9,13 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+     allitems=get_items()
+     return render_template("index.html",todo=allitems)
+     
+
+
+@app.route('/newitem' , methods=['POST'])
+def newitems():
+    Mynewtitle = request.form["title"] 
+    add_item(Mynewtitle)
+    return redirect('/')
