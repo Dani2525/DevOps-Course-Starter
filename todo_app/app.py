20bc8736d1a0018ba6fecd53d99b64c6a5d6e50b
DANI PHILIP <DaniPhilip5@outlook.com>
2022-01-04 22:21:32 +0000

    implementing the trello requests

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request,redirect
 from todo_app.data.session_items import get_items
 from todo_app.data.session_items import add_item
 from todo_app.flask_config import Config
+import requests
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -11,8 +12,8 @@ app.config.from_object(Config())
 def index():
      allitems=get_items()
      return render_template("index.html",todo=allitems)
-     
-
+     trello = requests.get('https://trello.com/b/byjuj5uu/todo')
+     trello.json()
 
 @app.route('/newitem' , methods=['POST'])
 def newitems():
