efdc2fbc6bf5b84cd51cbf6adf6e5b763bb2840b
DANI PHILIP <DaniPhilip5@outlook.com>
2022-10-27 17:19:12 +0100

    step 1 prep

--- todo_app/app.py
+++ todo_app/app.py
@@ -11,6 +11,7 @@ def create_app():
     app.config.from_object(Config())
 
     @app.route('/' , methods=['GET'])
+    @login_required 
     def alltodoitems():    
         allitems = mongo.getitems()
         todo_view_model = ViewModel(allitems)
@@ -18,16 +19,29 @@ def create_app():
 
 
     @app.route('/createnewcard' , methods=['POST'] )
+    @login_required 
     def createnewcard ():
         name = request.form["title"]
         mongo.createitem(name)
         return redirect('/')
 
-    
-
     @app.route('/complete_item/<id>', methods=['POST'])
+    @login_required 
     def complete_item(id):
         mongo.changestatus(mongo._id,mongo.status)
         return redirect('/')
 
+    login_manager = LoginManager()
+
+    @login_manager.unauthorized_handler
+    def unauthenticated():
+        
+        pass  # Add logic to redirect to the GitHub OAuth flow when unauthenticated
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        pass   # We will return to this later  
+    
+    login_manager.init_app(app)
+    
     return app    
