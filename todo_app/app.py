f38014986e84b67470a5e35b61f06d4f5c874e78
DANI PHILIP <DaniPhilip5@outlook.com>
2022-11-03 20:29:29 +0000

    API call to authenticate

--- todo_app/app.py
+++ todo_app/app.py
@@ -5,6 +5,7 @@ import os
 from todo_app.ViewModel import ViewModel,Item
 import todo_app.data.mongo_items as mongo
 from flask_login import login_required, LoginManager 
+import requests
 
 
 def create_app():
@@ -36,7 +37,16 @@ def create_app():
     def callback():
      code = request.args.get('code')
 
-     
+    data = {
+        'client_id': os.getenv('client_id'),
+        'client_secret': os.getenv('client_secret')
+    }
+    url = 'https://github.com/login/oauth/access_token'
+    headers = {'Accept': 'application/json'
+    }
+    
+    response = requests.post(url, data=data, headers= headers) 
+    access_token = response.json()['access_token']
     
     # then use "requests" to make the call to GitHub and swap the code for a token    
 
